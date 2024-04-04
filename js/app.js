209ce79130f599b866f45f5811310ddba22015f4
Eddier Lopez <eddier.lopez.lopez@una.cr>
2024-04-03 18:01:00 -0600

    Actualizacion de instalacion de SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+let url=window.location.href;
+let swLoc="unatask/service-worker.js";
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/service-worker.js')
+    if(url.includes('localhost')){
+        swLoc='/service-worker.js';
+    }
+    navigator.serviceWorker.register(swLoc)
         .then(reg=>{
             // setTimeout(()=>{
             //     reg.sync.register('post-new-user');

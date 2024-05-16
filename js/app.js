ada3d359284481aa7105d7144c78bc62da273a00
Eddier Lopez <eddier.lopez.lopez@una.cr>
2024-05-15 19:19:30 -0600

    Codigo actualizado

--- js/app.js
+++ js/app.js
@@ -12,3 +12,69 @@ if(navigator.serviceWorker){
             // },3000);
         });
 }
+const modalContainer=document.getElementById("modal-container")
+const addBtn=document.getElementById("add-task-button")
+
+addBtn.addEventListener("click",()=>{
+    createModal(`
+        <div class="container mt-5">
+            <h2 class="mb-4">Nueva tarea</h2>
+            <form id="new-task-form">
+                <div class="mb-3">
+                    <label for="nombre-tarea" class="form-label">Nombre de la tarea</label>
+                    <input type="text" class="form-control" id="nombre-tarea" name="nombre-tarea" required/>
+                </div>
+                <div class="mb-3">
+                    <label for="fecha-finalizacion" class="form-label">Fecha de finalización</label>
+                    <input type="date" class="form-control" id="fecha-finalizacion" name="fecha-finalizacion" required/>
+                </div>
+                <div class="mb-3">
+                    <label for="imagen" class="form-label">Captura de imagen</label>
+                    <input type="file" class="form-control" id="imagen" name="imagen"/>
+                </div>
+
+                <button type="submit" id="btn-save" class="btn btn-primary" >Guardar</button>
+            </form>
+        </div>
+    `)
+})
+
+const createModal=(content="")=>{
+    modalContainer.innerHTML=`
+        <div class="modal" id="modal">
+            <div class="modal-content modal-transform">
+                ${content}
+                <button id="btn-close-modal" class="modal-btn-closed">x</button>
+            </div>
+        </div>
+    `;
+    const deleteModal=()=>{
+        modalContainer.innerHTML=""
+    }
+    document.getElementById("modal").addEventListener("click",(e)=>{
+        if(e.target.id==="btn-close-modal"){
+            deleteModal()
+        }
+    })
+    const form=document.getElementById("new-task-form");
+    form.addEventListener("submit",saveTask);
+    
+    function saveTask(event){
+        event.preventDefault();
+        const data=new FormData(event.target);
+        const name=data.get("nombre-tarea");
+        const deadline=data.get("fecha-finalizacion");
+        const capture=null;
+        console.log(name);
+        createTask(name,deadline,capture);
+        deleteModal();
+        alert("Tarea creada")
+    }
+}
+
+window.onload=(e)=>{
+    //Metodo que verifique el token
+    //Si existe el token, cargamos las tareas
+    //Si no exite, debemos crear un modal (sin que cierre) solicitando el inicio de sesión 
+    console.log(getTasks());
+}
---
+++ js/services/task_service.js
@@ -0,0 +1,58 @@
+const Authorization="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhQXN4ZXFzZXJmc2QiLCJlbWFpbCI6ImVkZGllckB1bmEuY3IiLCJuYW1lIjoiRWRkaWVyIiwiaWF0IjoxNzE1ODE5NDY5fQ.ASo1uQWXgNdLubpvbBKbs5loYhQIaHkN2t9TxT_ehhc";
+const urlAPI="http://localhost:9000/graphql"
+
+const createTask=async (name,deadline,capture)=>{
+    const query=`
+        mutation($input: NewTaskInput!) {
+            createTask(input: $input) {
+            id
+            name
+            deadline
+            created_at
+            }
+        }      
+    `;
+    const input={
+        name,
+        deadline,
+        capture
+    };
+    return await fetchAPI(query,input);
+}
+
+const getTasks= async (limit)=>{
+    const query=`
+        query{
+            tasks {
+                items {                    
+                    deadline
+                    id
+                    name                    
+                }
+            }
+        }
+    `;
+    const input={
+        limit
+    };
+    return await fetchAPI(query,input);
+}
+const fetchAPI=async (query,input)=>{
+    const options={
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            Authorization
+        },
+        body: JSON.stringify({
+            query,
+            variables:{
+                input
+            }
+        })
+    };
+    const result=await fetch(urlAPI,options);
+    const data= await result.json();
+    console.log(data);
+    return data;
+}
